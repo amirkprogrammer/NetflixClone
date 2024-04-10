eb13bb6bf1e74904df7291184a5b251ad6735fb9
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-04-10 14:01:08 -0700

    - create homescreen and categories cell

---
+++ NetflixClone/Core/NetflixHomeView.swift
@@ -0,0 +1,49 @@
+//
+//  NetflixHomeView.swift
+//  NetflixClone
+//
+//  Created by Amir Kabiri on 4/10/24.
+//
+
+import SwiftUI
+
+struct NetflixHomeView: View {
+    var body: some View {
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+              header
+                    .padding(.horizontal, 16)
+                
+                Spacer()
+            }
+        }
+        .foregroundStyle(.netflixWhite)
+    }
+    
+    private var header: some View {
+        HStack(spacing: 0) {
+            Text("For You")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title)
+            
+            HStack {
+                Image(systemName: "tv.badge.wifi")
+                    .onTapGesture {
+                        
+                    }
+                
+                Image(systemName: "magnifyingglass")
+                    .onTapGesture {
+                        
+                    }
+            }
+            .font(.title2)
+        }
+    }
+}
+
+#Preview {
+    NetflixHomeView()
+}
